7644909e50cc12e8521bcaa761478e7d272fc9d6
MrZhouXR <1421867018@qq.com>
2020-11-08 20:05:28 +0800

    my标签组件

---
+++ component/tagList/tagList.js
@@ -0,0 +1,26 @@
+// component/tagList/tagList.js
+Component({
+  /**
+   * 组件的属性列表
+   */
+  properties: {
+    tagName:{
+      type: String,
+      value: '标题'
+    }
+  },
+
+  /**
+   * 组件的初始数据
+   */
+  data: {
+
+  },
+
+  /**
+   * 组件的方法列表
+   */
+  methods: {
+
+  }
+})
