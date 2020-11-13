db59548dcbc115a4eca55e2329230f4d3c34f227
MrZhouXR <1421867018@qq.com>
2020-11-13 20:32:45 +0800

    购物车更新

--- pages/ShoppingCart/ShoppingCart.js
+++ pages/ShoppingCart/ShoppingCart.js
@@ -5,7 +5,117 @@ Page({
    * 页面的初始数据
    */
   data: {
-
+    emptycart: true,
+    goodsList:[
+      {
+        imgName: '灵越笔记本.jpg',
+        tagName: '双11.png',
+        goodsDesc: '戴尔（DELL）游匣新g3/g5/g7灵越5500 15.6英寸i5/i7学生游戏电竞笔记本电脑 十代i5 GTX1650Ti 120Hz电竞屏 16内存512G PCIe固态 【高配版】',
+        price: 6666,
+        mod: '满减',
+        recommendNum: 666,
+        recommendRate: '100%'
+      },
+      {
+        imgName: '灵越笔记本.jpg',
+        tagName: '双11.png',
+        goodsDesc: '戴尔（DELL）游匣新g3/g5/g7灵越5500 15.6英寸i5/i7学生游戏电竞笔记本电脑 十代i5 GTX1650Ti 120Hz电竞屏 16内存512G PCIe固态 【高配版】',
+        price: 6666,
+        mod: '满减',
+        recommendNum: 666,
+        recommendRate: '100%'
+      },
+      {
+        imgName: '灵越笔记本.jpg',
+        tagName: '双11.png',
+        goodsDesc: '戴尔（DELL）游匣新g3/g5/g7灵越5500 15.6英寸i5/i7学生游戏电竞笔记本电脑 十代i5 GTX1650Ti 120Hz电竞屏 16内存512G PCIe固态 【高配版】',
+        price: 6666,
+        mod: '满减',
+        recommendNum: 666,
+        recommendRate: '100%'
+      },
+      {
+        imgName: '灵越笔记本.jpg',
+        tagName: '双11.png',
+        goodsDesc: '戴尔（DELL）游匣新g3/g5/g7灵越5500 15.6英寸i5/i7学生游戏电竞笔记本电脑 十代i5 GTX1650Ti 120Hz电竞屏 16内存512G PCIe固态 【高配版】',
+        price: 6666,
+        mod: '满减',
+        recommendNum: 666,
+        recommendRate: '100%'
+      },
+      {
+        imgName: '灵越笔记本.jpg',
+        tagName: '双11.png',
+        goodsDesc: '戴尔（DELL）游匣新g3/g5/g7灵越5500 15.6英寸i5/i7学生游戏电竞笔记本电脑 十代i5 GTX1650Ti 120Hz电竞屏 16内存512G PCIe固态 【高配版】',
+        price: 6666,
+        mod: '满减',
+        recommendNum: 666,
+        recommendRate: '100%'
+      },
+      {
+        imgName: '灵越笔记本.jpg',
+        tagName: '双11.png',
+        goodsDesc: '戴尔（DELL）游匣新g3/g5/g7灵越5500 15.6英寸i5/i7学生游戏电竞笔记本电脑 十代i5 GTX1650Ti 120Hz电竞屏 16内存512G PCIe固态 【高配版】',
+        price: 6666,
+        mod: '满减',
+        recommendNum: 666,
+        recommendRate: '100%'
+      },
+      {
+        imgName: '灵越笔记本.jpg',
+        tagName: '双11.png',
+        goodsDesc: '戴尔（DELL）游匣新g3/g5/g7灵越5500 15.6英寸i5/i7学生游戏电竞笔记本电脑 十代i5 GTX1650Ti 120Hz电竞屏 16内存512G PCIe固态 【高配版】',
+        price: 6666,
+        mod: '满减',
+        recommendNum: 666,
+        recommendRate: '100%'
+      },
+      {
+        imgName: '灵越笔记本.jpg',
+        tagName: '双11.png',
+        goodsDesc: '戴尔（DELL）游匣新g3/g5/g7灵越5500 15.6英寸i5/i7学生游戏电竞笔记本电脑 十代i5 GTX1650Ti 120Hz电竞屏 16内存512G PCIe固态 【高配版】',
+        price: 6666,
+        mod: '满减',
+        recommendNum: 666,
+        recommendRate: '100%'
+      },
+      {
+        imgName: '灵越笔记本.jpg',
+        tagName: '双11.png',
+        goodsDesc: '戴尔（DELL）游匣新g3/g5/g7灵越5500 15.6英寸i5/i7学生游戏电竞笔记本电脑 十代i5 GTX1650Ti 120Hz电竞屏 16内存512G PCIe固态 【高配版】',
+        price: 6666,
+        mod: '满减',
+        recommendNum: 666,
+        recommendRate: '100%'
+      },
+      {
+        imgName: '灵越笔记本.jpg',
+        tagName: '双11.png',
+        goodsDesc: '戴尔（DELL）游匣新g3/g5/g7灵越5500 15.6英寸i5/i7学生游戏电竞笔记本电脑 十代i5 GTX1650Ti 120Hz电竞屏 16内存512G PCIe固态 【高配版】',
+        price: 6666,
+        mod: '满减',
+        recommendNum: 666,
+        recommendRate: '100%'
+      },
+      {
+        imgName: '灵越笔记本.jpg',
+        tagName: '双11.png',
+        goodsDesc: '戴尔（DELL）游匣新g3/g5/g7灵越5500 15.6英寸i5/i7学生游戏电竞笔记本电脑 十代i5 GTX1650Ti 120Hz电竞屏 16内存512G PCIe固态 【高配版】',
+        price: 6666,
+        mod: '满减',
+        recommendNum: 666,
+        recommendRate: '100%'
+      },
+      {
+        imgName: '灵越笔记本.jpg',
+        tagName: '双11.png',
+        goodsDesc: '戴尔（DELL）游匣新g3/g5/g7灵越5500 15.6英寸i5/i7学生游戏电竞笔记本电脑 十代i5 GTX1650Ti 120Hz电竞屏 16内存512G PCIe固态 【高配版】',
+        price: 6666,
+        mod: '满减',
+        recommendNum: 666,
+        recommendRate: '100%'
+      }
+    ]
   },
 
   /**
@@ -47,7 +157,17 @@ Page({
    * 页面相关事件处理函数--监听用户下拉动作
    */
   onPullDownRefresh: function () {
-
+    
+  //下拉刷新
+    wx.showNavigationBarLoading() //在标题栏中显示加载
+    
+    //模拟加载
+    setTimeout(function()
+    {
+      // complete
+      wx.hideNavigationBarLoading() //完成停止加载
+      wx.stopPullDownRefresh() //停止下拉刷新
+    },1500);
   },
 
   /**
