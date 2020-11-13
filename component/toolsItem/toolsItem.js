53eacc32d580676509817dfc8ccabc647e9cbb2c
MrZhouXR <1421867018@qq.com>
2020-11-13 20:49:11 +0800

    活动组件

---
+++ component/toolsItem/toolsItem.js
@@ -0,0 +1,30 @@
+// component/toolsItem/toolsItem.js
+Component({
+  /**
+   * 组件的属性列表
+   */
+  properties: {
+    iconName: {
+        type: String,
+        value: '图片名称'
+    },
+    iconTitle:{
+        type: String,
+        value: '图标标题'
+    }
+  },
+
+  /**
+   * 组件的初始数据
+   */
+  data: {
+
+  },
+
+  /**
+   * 组件的方法列表
+   */
+  methods: {
+
+  }
+})
